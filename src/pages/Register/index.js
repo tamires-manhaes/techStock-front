2aebf3a6677588ddad25708006a9cb96e2c6a148
Tamires Manhães <tamiresmanhaes4@gmail.com>
2020-07-17 18:29:42 -0300

    ajustando texto de link

--- src/pages/Register/index.js
+++ src/pages/Register/index.js
@@ -38,7 +38,7 @@ export default function Register(){
 
           <Link className="back-link" to="/">
             <FiArrowLeft size={16} color="#E02041" />
-            Não tenho cadastro
+            Voltar para login
           </Link>
         </section>
 
